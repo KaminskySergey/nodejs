8e54b1901f30d3fd32ddeb97a5d1b7a87b281798
KaminskySergey <kaminsky_s_s@ukr.net>
2023-03-26 14:02:55 +0300

    favorite

--- controllers/contacts/getContact.js
+++ controllers/contacts/getContact.js
@@ -5,9 +5,9 @@ console.log(Contact)
 const getContact = async (req, res, next) => {
     const {_id} = req.user
     
-    const {page = 1, limit = 10} = req.query
+    const {page = 1, limit = 10, favorite = true} = req.query
     const skip = (page - 1) * limit;
-    const getListAll = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate('owner', "email subscription")
+    const getListAll = await Contact.find({owner: _id, favorite }, "", {skip, limit: Number(limit)}).populate('owner', "email subscription")
     res.status(200).json({ message: getListAll, status: "success" })
     
   }
